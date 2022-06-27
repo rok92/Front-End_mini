33a9246808b300cd66e234a642ad06371942eed6
Hyewon <hwwwon1027@gmail.com>
2022-06-27 15:03:47 +0900

    add index, flight_main file

---
+++ js/air_main.js
@@ -0,0 +1,102 @@
+// 데이터 피커 수정 jQuery
+
+var now = new Date();
+var today = (now.getMonth()+1) + "/" + now.getDate() + "/" + now.getFullYear();
+
+$(function(){
+
+  $("#rangepicker").daterangepicker({
+    locale: {
+      daysOfweek: ['일','월','화','수','목','금','토'],
+      monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월']
+    },
+    
+    autoUpdateInput: false,
+    autoApply: true,
+    minDate: today
+  });
+
+});
+
+
+$('input[name="daterange"]').on('apply.daterangepicker', function(ev,picker){
+  $(this).val(picker.startDate.format('MM.DD(dd)') + ' ~ ' + picker.endDate.format('MM.DD(dd)'));
+});
+
+
+let airDateBox = document.getElementById('rangepicker');
+airDateBox.addEventListener('click',()=>{
+  airDateBox.value = "여행 날짜 선택";
+});
+
+
+
+$(document).ready(function(){
+
+  // top 버튼
+  $('#topBtn').on('click',function(){
+
+    $('html, body').animate({scrollTop:0},500);
+  });
+
+
+  
+
+});
+
+
+// 출발지 선택하는 버튼
+
+let departLocation = document.getElementById("depart_loacation");
+let departPopUP = document.getElementById("depart_pop");
+let count = 1;
+
+departLocation.addEventListener("click",clickCount);
+
+function clickCount(){
+  count++;
+  
+  if(count % 2 == 0){
+    departPopUP.style.display = "block";
+  }else{
+    departPopUP.style.display = "none";
+  }
+}
+
+//도착지 선택하는 버튼
+let arriveLocation = document.getElementById("arrive_location");
+let arrivePopUP = document.getElementById("arrive_pop");
+let count2 = 1;
+arriveLocation.addEventListener("click",clickCount2);
+
+function clickCount2(){
+  count2++;
+  
+  if(count2 % 2 == 0){
+    arrivePopUP.style.display = "block";
+  }else{
+    arrivePopUP.style.display = "none";
+  }
+}
+
+//인원수 및 좌석 선택 버튼
+let personSit = document.getElementById("person_sit");
+let customAirPOP = document.getElementById("custom_air_pop");
+let count3 = 1;
+personSit.addEventListener("click",clickCount3);
+
+function clickCount3(){
+  count3++;
+  
+  if(count3 % 2 == 0){
+    customAirPOP.style.display = "block";
+  }else{
+    customAirPOP.style.display = "none";
+  }
+}
+
+
+
+
+
+
