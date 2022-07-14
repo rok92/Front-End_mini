c225f7cc4e0137668c24ae283fc92987514e66eb
Shuroop2 <kingdho3o@gmail.com>
2022-07-14 23:26:23 +0900

    fix flight_main file

--- js/flight_main.js
+++ js/flight_main.js
@@ -58,6 +58,30 @@ function clickCount2(){
   }
 }
 
+// // 호범낭룸ㅇㄹ
+// $('#depart_location_table td').hover(
+//   function(){
+//   $(this).addClass('bg_gray');
+//   },
+//   function(){
+//   $(this).removeClass('bg_gray');
+//   }
+// );
+
+// 도시 선택 시 출발지 / 도착지 자동 입력
+// 출발지
+var city;
+$('#depart_location_table td').click(function(){
+  city = $(this).text();
+  $('#depart_loacation').attr('value', city);
+});
+
+// 도착지
+$('#arrive_location_table td').click(function(){
+  city = $(this).text();
+  $('#arriveLocation').attr('value', city);
+});
+
 //인원수 및 좌석 선택 버튼 block/none
 let personSit = document.getElementById("person_sit");
 let customflightPOP = document.getElementById("custom_flight_pop");
@@ -123,58 +147,177 @@ shuttleInit();
 // 출발지 여행지 선택
 // 출발지 팝업 내 검색하기 버튼 클릭 시
 $('#popupLodSearchBtn').click(function(){
-  var txt = $('#popupLodSearch').val() + " (ICN)";
+  var txt = $('#popupLodSearch').val();
   if(txt == ''){
     alert("여행지를 입력하세요");
   }else{
     $('#depart_loacation').attr('value', txt);
-    $('#depart_loacation').removeClass('txt_gray');
     $('.depart_popup').css('display','none');
   }
 });
 
+$("#popupLodSearch").on("keyup",function(key){
+	if(key.keyCode==13){
+	  var txt = $('#popupLodSearch').val();
+    if(txt == ''){
+      alert("여행지를 입력하세요");
+    }else{
+      $('#depart_loacation').attr('value', txt);
+      $('.depart_popup').css('display','none');
+    }
+	}
+});
+
 // 도착지 팝업 내 검색하기 버튼 클릭 시
 $('#popupLodSearchBtn2').click(function(){
-  var txt = $('#popupLodSearch2').val() + " (GUM)";
+  var txt = $('#popupLodSearch2').val();
   if(txt == ''){
     alert("여행지를 입력하세요");
   }else{
     $('#arriveLocation').attr('value', txt);
-    $('#arriveLocation').removeClass('txt_gray');
     $('.arrive_popup').css('display','none');
   }
 });
 
+$("#popupLodSearch2").on("keyup",function(key){
+	if(key.keyCode==13){
+	  var txt = $('#popupLodSearch2').val();
+    if(txt == ''){
+      alert("여행지를 입력하세요");
+    }else{
+      $('#arriveLocation').attr('value', txt);
+      $('.arrive_popup').css('display','none');
+    }
+	}
+});
+
 // 인원 좌석 선택 버튼 클릭
-//성인
 let adultBtnMinus = document.getElementById('adultBtnMinus');
 let personCount = document.getElementById('personCount');
 let classType = document.getElementById('classType');
+let maxNumber = 1;
 
 function plusMinusBtn(pm){
   const adCount = document.getElementById('adCount');
-  let number = adCount.innerHTML;
+  const cdCount = document.getElementById('cdCount');
+  const bbCount = document.getElementById('bbCount');
+  let adNumber = parseInt(adCount.innerHTML);
+  let cdNumber = parseInt(cdCount.innerHTML);
+  let bbNumber = parseInt(bbCount.innerHTML);
 
-  if(pm == "plus"){
-    number = parseInt(number) + 1;
-  }else if(pm = "minus"){
-    number = parseInt(number) - 1;
+  // 인원 선택 버튼
+  if(pm == "adplus"){
+    if(adNumber < 9) {
+      adNumber = adNumber + 1;
+      maxNumber = maxNumber + 1;
+      if(maxNumber >= 10){
+        maxNumber = 9;
+        return false; 
+      }
+    }
+  }else if(pm == "adminus"){
+    if(adNumber > 1){
+      adNumber = adNumber - 1;
+      maxNumber = maxNumber - 1;
+    }
+  }else if(pm == "cdplus"){
+    if(cdNumber < 9) {
+      cdNumber = cdNumber + 1;
+      maxNumber = maxNumber + 1;
+      if(maxNumber >= 10){
+        maxNumber = 9;
+        return false; 
+      }
+    }
+  }else if(pm == "cdminus"){
+    if(cdNumber > 0){
+      cdNumber = cdNumber - 1;
+      maxNumber = maxNumber - 1;
+    }
+  }else if(pm == "bbplus"){
+    if(bbNumber < 9) {
+      bbNumber = bbNumber + 1;
+      maxNumber = maxNumber + 1;
+      if(maxNumber >= 10){
+        maxNumber = 9;
+        return false; 
+      }
+    }
+  }else if(pm == "bbminus"){
+    if(bbNumber > 0){
+      bbNumber = bbNumber - 1;
+      maxNumber = maxNumber - 1;
+    }
   }
 
-  adCount.innerHTML = number;
-  personCount.value = number;
+  adCount.innerHTML = adNumber;
+  cdCount.innerHTML = cdNumber;
+  bbCount.innerHTML = bbNumber;
+  personCount.value = maxNumber + "명";
   
+  // 버튼 비활성화
   if(adCount.innerHTML > 1){
-    adultBtnMinus.setAttribute("disabled", false);
+    $('#adultBtnMinus').css("color", "#48A0FF");
+    $('#adultBtnMinus').css("border-color", "#48A0FF");
+  } else if (adCount.innerHTML == 1) {
+    $('#adultBtnMinus').css("color", "#BBBBBB");
+    $('#adultBtnMinus').css("border-color", "#BBBBBB");
+  } 
+
+  if (adCount.innerHTML == 9) {
+    $('#adultBtnPlus').css("color", "#BBBBBB");
+    $('#adultBtnPlus').css("border-color", "#BBBBBB");
+  } else if (adCount.innerHTML < 9) {
+    $('#adultBtnPlus').css("color", "#48A0FF");
+    $('#adultBtnPlus').css("border-color", "#48A0FF");
   }
 
+  if(cdCount.innerHTML > 0){
+    $('#childBtnMinus').css("color", "#48A0FF");
+    $('#childBtnMinus').css("border-color", "#48A0FF");
+  } else if (cdCount.innerHTML == 0) {
+    $('#childBtnMinus').css("color", "#BBBBBB");
+    $('#childBtnMinus').css("border-color", "#BBBBBB");
+  } 
+
+  if (cdCount.innerHTML == 9) {
+    $('#childBtnPlus').css("color", "#BBBBBB");
+    $('#childBtnPlus').css("border-color", "#BBBBBB");
+  } else if (cdCount.innerHTML < 9) {
+    $('#childBtnPlus').css("color", "#48A0FF");
+    $('#childBtnPlus').css("border-color", "#48A0FF");
+  }
+
+  if(bbCount.innerHTML > 0){
+    $('#babyBtnMinus').css("color", "#48A0FF");
+    $('#babyBtnMinus').css("border-color", "#48A0FF");
+  } else if (bbCount.innerHTML == 0) {
+    $('#babyBtnMinus').css("color", "#BBBBBB");
+    $('#babyBtnMinus').css("border-color", "#BBBBBB");
+  } 
+
+  if (bbCount.innerHTML == 9) {
+    $('#babyBtnPlus').css("color", "#BBBBBB");
+    $('#babyBtnPlus').css("border-color", "#BBBBBB");
+  } else if (bbCount.innerHTML < 9) {
+    $('#babyBtnPlus').css("color", "#48A0FF");
+    $('#babyBtnPlus').css("border-color", "#48A0FF");
+  }
+
+  if(maxNumber == 9){
+    $('#adultBtnPlus').css("color", "#BBBBBB");
+    $('#adultBtnPlus').css("border-color", "#BBBBBB");
+    $('#childBtnPlus').css("color", "#BBBBBB");
+    $('#childBtnPlus').css("border-color", "#BBBBBB");
+    $('#babyBtnPlus').css("color", "#BBBBBB");
+    $('#babyBtnPlus').css("border-color", "#BBBBBB");
+  }
 }
 
-function plusMinusInit(){
-  adultBtnMinus.setAttribute('disabled', true);
-}
-plusMinusInit();
-
+// 좌석 선택
+$('input[name="sits"]').on('click', function(){
+  classType.value = $('input:radio[name="sits"]:checked').val();
+});
 
 //유효성검사, 링크
 let flightDateBox = document.getElementById('rangepicker');
@@ -255,15 +398,15 @@ function rctInit(){
 
 rctInit();
 
-//최근 검색한 항공권 슬라이드 내용 지우기 (아직 못함)
+//최근 검색한 항공권 슬라이드 내용 지우기
 let rctBox = document.querySelectorAll(".recently_slide_box > div");
-
-function removeFlightBox(){
-  for(let i = 0; i < rctSlideCount.length; i++){
-    rctBox[i].remove();
-    rctPrevButton();
+$('.recently_colse_btn').click(function(){
+  $(this).closest('.recently_item').remove();
+  if($('.recently_item').length == 3){
+    rctNext.style.visibility = "hidden";
+    rctPrev.style.visibility = "hidden";
   }
-}
+});
 
 // 탑 버튼 눌렀을 때 최상단으로
 $(".btn_top").click(function () {
@@ -272,7 +415,3 @@ $(".btn_top").click(function () {
   }, 400);
   return false;
 });
-
-
-
-
